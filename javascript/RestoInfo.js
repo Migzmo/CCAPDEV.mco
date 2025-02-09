611c55bcec27192ce696597c37e0b81e2b92c572
adlerscodes <adlerestrebel@gmail.com>
2025-02-09 13:48:15 +0800

    Updated the css of RestoInfo

---
+++ javascript/RestoInfo.js
@@ -0,0 +1,8 @@
+function togglePopup() {
+    var popup = document.getElementById("reviewPopup");
+    if (popup.style.display === "block") {
+        popup.style.display = "none";
+    } else {
+        popup.style.display = "block";
+    }
+}
