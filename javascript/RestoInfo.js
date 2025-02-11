d533e4bbe1a7e7b34a56c39ff8a28cd5603f984b
adlerscodes <adlerestrebel@gmail.com>
2025-02-11 20:45:05 +0800

    Updated RestoInfo

--- javascript/RestoInfo.js
+++ javascript/RestoInfo.js
@@ -1,8 +1,21 @@
-function togglePopup() {
-    var popup = document.getElementById("reviewPopup");
-    if (popup.style.display === "block") {
-        popup.style.display = "none";
-    } else {
-        popup.style.display = "block";
-    }
+// Get the modal and button elements
+const modal = document.getElementById("myModal");
+const btn = document.getElementById("openModal");
+const span = document.getElementsByClassName("close")[0];
+
+// Open the modal when the button is clicked
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// Close the modal when the "x" is clicked
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// Close the modal if the user clicks outside of it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
