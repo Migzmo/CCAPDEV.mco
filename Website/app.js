416b2e028e5349c2998043006d9ae2aaef902c5f
adlerscodes <adlerestrebel@gmail.com>
2025-03-10 18:37:05 +0800

    Added temporary code in app.js and revised the favorite button per restaurant

--- Website/app.js
+++ Website/app.js
@@ -16,6 +16,34 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname));
 
+
+// // !!! TEMP CODE !!!
+
+const restoList = [
+    { id: 1, name: "El Poco", file: "RestoElPoco.html" },
+    { id: 2, name: "Jollibee", file: "RestoJollibee.html" },
+    { id: 3, name: "Mang Inasal", file: "RestoMangInasal.html" },
+    { id: 4, name: "McDonald's", file: "Restomcdowebp.html" },
+    { id: 5, name: "Pho Mahal", file: "RestoPhoMahal.html" }
+]
+app.get('/restaurant_pages', function (req, res) {
+    res.json(restoList);
+});
+
+// Dynamic route to serve restaurant pages
+app.get("/restaurant/:file", (req, res) => {
+    const fileName = req.params.file;
+    const filePath = path.join(__dirname, "html", fileName);
+  
+    res.sendFile(filePath, (err) => {
+      if (err) {
+        res.status(404).send("Restaurant page not found.");
+      }
+    });
+  });
+// /// ======================
+
+
 //This blocks access to the database for now idk yet
 app.use('/database', function (req, res, next) {
     res.status(403).send('Access denied');
