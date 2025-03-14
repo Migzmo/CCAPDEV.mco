3c5668ed6c769c33d4af79fba35c50273448a3fd
Migzmo <josemiguelespinosa74@gmail.com>
2025-03-14 22:09:13 +0800

    Updated w/ Add Resto UI

--- Website/JavaScript/directoryScript.js
+++ Website/JavaScript/directoryScript.js
@@ -12,3 +12,18 @@ function togglePopup() {
         document.body.style.pointerEvents = 'auto';
     }
 }
+
+function togglePopupCreateResto() {
+    const popup = document.getElementById('createRestoFrame');
+    const backdrop = document.getElementById('backdrop');
+    const isHidden = (popup.style.display === 'none');
+    popup.style.display = isHidden ? 'block' : 'none';
+    backdrop.style.display = isHidden ? 'block' : 'none';
+
+    if (isHidden) {
+        document.body.style.pointerEvents = 'none';
+        popup.style.pointerEvents = 'auto';
+    } else {
+        document.body.style.pointerEvents = 'auto';
+    }
+}
