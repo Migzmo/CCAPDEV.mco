7d6b0c9c4127846da6eeedec3a20c623d8ee761b
Migzmo <josemiguelespinosa74@gmail.com>
2025-03-15 02:49:40 +0800

    Removed redundant icons

--- Website/app.js
+++ Website/app.js
@@ -18,7 +18,7 @@ const fs = require('fs');
 
 //For Initializing express
 const express = require('express');
-const fileUpload = require('express-fileupload')
+const fileUpload = require('express-fileupload');
 const path = require('path');
 const app = express();
 
